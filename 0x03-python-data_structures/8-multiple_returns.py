66e2c9319e25a25cd8b75d85a62b6c9f2a31cf33
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-11-04 05:46:39 -0700

    Fixed pydecostyle stuffy and chmod

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        tupi = (0, "None")
-    else: 
-        tupi=(len(sentence), sentence[0])
-    return(tupi)
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
