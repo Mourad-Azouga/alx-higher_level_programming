d95b152c29b8c920b218398148a86951705378ae
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-11-04 06:05:23 -0700

    Fixed Pycode stuff and stuff

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return 'None'
     else:
         max = my_list[0]
-        for i in range (len(my_list) - 1):
+        for i in range(len(my_list) - 1):
             if max < my_list[i]:
                 max = my_list[i]
     return(max)
