c8bdc9470d16d12b85bec088a16ba42c7c2b8d42
Mourad Azouga <mourad-azouga@outlook.fr>
2023-11-28 18:17:18 +0100

    Made the directory and added some stuff

--- 0-rectangle.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Defining rectangle class"""
-
-class Rectangle:
-    """this should do it I guess"""
-    pass
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+This is a README for the project 0x09-python-everything_is_object
