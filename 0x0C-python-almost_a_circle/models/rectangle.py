3163dbf6f0341a49c65d84b831d1822dec0413b7
Mourad Azouga <mourad-azouga@outlook.fr>
2023-12-11 18:37:56 +0100

    Some progress

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Let's make a rectangle class"""
+"""Defgines a rectangle class"""
 from models.base import Base
+
+
+
 class Rectangle(Base):
-    """Let's make a Rectangle"""
+    """Represents a rectangle"""
     def __init__(self, width, height, x = 0, y = 0, id = None):
-        """This function will initialize the Rectangle
+        """Initialize a new Rectangle.
+
         Args:
-            width (int) = width
-            height (int) = height
-            x (int) = x axis 
-            y (int) = y axis
-            id (int) = the id"""
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
