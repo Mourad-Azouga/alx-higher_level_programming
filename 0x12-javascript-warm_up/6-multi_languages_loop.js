bacc21606e6faca29c4fb34b8501e7b6d43f94d8
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-02-12 10:56:03 -0800

    Fixed the console ammount usage in 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 let i;
 for (i = 0; i < 3; i++) {
-  console.log('C is fun');
-  console.log('Python is cool');
-  console.log('JavaScript is amazing');
+  console.log('C is fun\nPython is cool\nJavaScript is amazing');
 }
