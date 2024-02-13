b0f7095bb4e822f249387a75fe95647a8aa00246
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-02-13 04:34:00 -0800

    Added test main and fixed the code by changing h = undefined to this.height = undefined

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      w = undefined;
-      h = undefined;
-    }
+      this.width = undefined;
+      this.height = undefined;
+    } else {
     this.width = w;
     this.height = h;
+  }
   }
 }
 module.exports = Rectangle;
