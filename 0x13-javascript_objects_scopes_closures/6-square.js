ff24a3d15bf0bf854462848ce4ab955e26f50058
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-02-13 10:02:38 -0800

    Added 6 and seven

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Squar = require('./5-square');
+class Square extends Squar {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
