e4c749f76415ca69a9adca14de832ab94c8b3876
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-02-16 10:11:44 -0800

    Added 3, turns out I already made un-injectable before

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa 
+Where the name is searchable by the 4th argv"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    filter = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s"
+            ,(filter,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
