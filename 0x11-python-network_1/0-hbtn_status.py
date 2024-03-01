240701b219004eaaf092b8fe72be33c629a15f89
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-01 22:46:15 +0100

    Fixed the documentation on 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 fetches https://alx-intranet.hbtn.io/status
+Using utllib package
+Displaying the informations in a specific manner
 """
 if __name__ == "__main__":
     import urllib.request
@@ -10,3 +12,4 @@ if __name__ == "__main__":
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    
