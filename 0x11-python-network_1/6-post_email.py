0fd20d6a41e9af5f980c5134339fe2accc4b9217
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-01 23:22:21 +0100

    Automatic pusher

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A script
-- takes in a URL,
-- sends a request to the URL and
-- disp  X-Request-Id var found in the header.
+"""a little python thingy that
+Sends a POST request to a given URL with a given email.
 """
 import sys
-import urllib.request
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.post(url, data=value)
+    print(r.text)
