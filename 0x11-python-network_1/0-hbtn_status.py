2d280b11da3252a98e86c1ca2acd7aec7b9b45cc
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-01 23:02:12 +0100

    Automatic pusher

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:        
-        html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
