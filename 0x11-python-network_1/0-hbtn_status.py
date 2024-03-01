2f4b72ac6a57c748dde740f397fe9e75737ce20e
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-01 22:47:35 +0100

    Automatic pusher

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ fetches https://alx-intranet.hbtn.io/status
 Using utllib package
 Displaying the informations in a specific manner
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
