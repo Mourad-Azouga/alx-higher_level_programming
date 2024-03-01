13333da22ae0ddff6920de465fe3f7443872bf75
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-01 23:10:36 +0100

    Automatic pusher

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""uses requests to find the status of the url"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
