9e8fe3832a920cba63c2a220717825aca0498ad8
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-02 18:33:41 +0100

    Added 6-main, updated 6 and 6.txt, updated 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a POST request
-curl -sX POST $1 -d "email:test@gmail.com" -d"subject:I will always be here for PLD" -L 
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
